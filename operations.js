c440eb2b1f8a99a0c0b1189ac7e7483f5b0cb799
SiddhartJha <103504676+SiddhartJha@users.noreply.github.com>
2024-04-20 11:22:02 +0000

    operations

---
+++ operations.js
@@ -0,0 +1,20 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+const str1 = "hello"
+const str2 = " siddharth"
+const str3 = str1+str2
+console.log(str3);
+console.log("1"+2)
+console.log(1+"2")
+//
+console.log("1"+2+2)   // yha string phle h isliye string add nhi ho rha h
+console.log(1+2+"3")  //yha string baad mein h isliye phle add ho rha h
+
+console.log(+true);
+console.log(+"")
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
