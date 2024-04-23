690aeea1286761ecfdea2d08c45dc146518920a1
SiddhartJha <103504676+SiddhartJha@users.noreply.github.com>
2024-04-23 07:02:52 +0000

    arrays

---
+++ arrays.js
@@ -0,0 +1,33 @@
+// const myarr = [1,2,3,4,5]
+
+// console.log("A", myarr)
+
+// console.log(myarr.slice(1,3)) //in slice method original array does not change
+
+// console.log("B", myarr)
+
+// console.log(myarr.splice(1,3)) //in splice method original array gets changed
+
+// console.log("C", myarr)
+
+// const marvel_heroes = ["ironman", "captain", "thor", "spidrman"]
+// const dc_heroes = ["superman", "flash", "batman"]
+
+// const all_heroes = marvel_heroes.concat(dc_heroes) // returns a new array of copied cancatted values
+
+// console.log(all_heroes)
+
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes] //spread operator
+// console.log(all_new_heroes)
+// console.log(marvel_heroes)
+
+// const another_array = [1,2,[3,4,5],5,7,[6,8,[3,1]]]
+
+// const modified_array = another_array.flat(Infinity)
+// console.log(modified_array)
+
+
+// console.log(typeof("hitesh"))
+// console.log(Array.from("hitesh"))
+
+
